4c5696c0039ec28f0ac8f5612ee95b550e099287
ŃaSrO <bouaouina.nasreddine@gmail.com>
2021-02-23 22:57:23 +0100

    adding the User

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const genres = require("./routes/genres");
 const customers = require("./routes/customers");
 const movies = require("./routes/movies");
 const rentals = require("./routes/rentals");
-
+const users = require("./routes/user");
 const Joi = require("joi");
 
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://localhost/vidly")
+  .connect("mongodb://localhost/vidbib")
   .then(() => console.log("Connected to Mongodb"))
   .catch((err) => console.error("Could not connect to MongoDb"));
 
@@ -19,6 +19,6 @@ app.use("/api/genres", genres);
 app.use("/api/customers", customers);
 app.use("/api/movies", movies);
 app.use("/api/rentals", rentals);
-
+app.use("/api/users", users);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on Port ${PORT}`));
---
+++ routes/user.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+const { User, validate } = require("../models/user");
+const _ = require("lodash");
+const bcrypt = require("bcrypt");
+
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(404).send(error.details[0].message);
+
+  // Verifying if the User already exist in the DB
+  let user = await User.findOne({ email: req.body.email });
+  if (user) return res.status(400).send("User already rgistered");
+
+  // old Methode
+  //   user = new User({
+  //     name: req.body.name,
+  //     email: req.body.email,
+  //     password: req.body.password,
+  //   });
+
+  // New Method with Lodash
+  user = new User(_.pick(req.body, ["name", "email", "password"]));
+
+  // Hashing The Password using Bcrypt
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
+
+  await user.save();
+
+  res.send(_.pick(user, ["_id", "name", "email"]));
+});
+
+module.exports = router;
